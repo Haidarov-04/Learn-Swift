12986602171ef72263c0258552c8d4edf905d69e
haidarov_n <nozimjonhaydarov6@gmail.com>
2025-02-10 12:07:23 +0500

    update

---
+++ Learn Swift/View/DetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailVC.swift
+//  Learn Swift
+//
+//  Created by Haidarov N on 2/7/25.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
